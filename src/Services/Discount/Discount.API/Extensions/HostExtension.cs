8d309f0a81053a0fb12aab7fa3596ac8678283fa
TamNguyen <tamkid2408@gmail.com>
2021-11-15 13:10:43 +0700

    Add migrate postgres db for Discount service

---
+++ src/Services/Discount/Discount.API/Extensions/HostExtension.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+using System.Threading;
+
+namespace Discount.API.Extensions
+{
+    public static class HostExtension
+    {
+        public static IHost MigrateDatabase<TContext>(this IHost host, int? retry = 0)
+        {
+            int retryForAbility = retry.Value;
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var configuration = services.GetRequiredService<IConfiguration>();
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+
+                try
+                {
+                    logger.LogInformation("Migrating postgres database.");
+
+                    using (var connection = new NpgsqlConnection(configuration.GetValue<string>("DatabaseSettings:ConnectionString")))
+                    {
+                        connection.Open();
+
+                        using (var command = new NpgsqlCommand() { Connection = connection })
+                        {
+                            command.CommandText = "DROP TABLE IF EXISTS Coupon";
+                            command.ExecuteNonQuery();
+
+                            command.CommandText = @"CREATE TABLE Coupon(
+	                                                    ID SERIAL PRIMARY KEY NOT NULL,
+	                                                    ProductName VARCHAR(24) NOT NULL,
+	                                                    Description	TEXT,
+	                                                    Amount	INT
+                                                    )";
+                            command.ExecuteNonQuery();
+
+                            command.CommandText = @"INSERT INTO Coupon(ProductName, Description, Amount) VALUES ('IPhone X', 'Iphone discount', 150)";
+                            command.ExecuteNonQuery();
+
+                            command.CommandText = @"INSERT INTO Coupon(ProductName, Description, Amount) VALUES ('Samsung 10', 'Samsung discount', 100)";
+                            command.ExecuteNonQuery();
+                        }
+                    }
+
+                    logger.LogInformation("Migrated postgres database.");
+                }
+                catch (NpgsqlException e)
+                {
+                    logger.LogError(e, "An error occurred while migrating the postgres database.");
+
+                    if (retryForAbility < 50)
+                    {
+                        retryForAbility++;
+                        Thread.Sleep(2000);
+                        MigrateDatabase<TContext>(host, retryForAbility);
+                    }
+                }
+            }
+
+            return host;
+        }
+    }
+}
